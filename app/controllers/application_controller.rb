f488d3091596b9f3673b1a1207c09b9f36577989
Natasha K <tashk85@gmail.com>
2019-04-23 15:59:38 +1000

    added user authorization to edit pages and login/logout functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    private
+    def after_sign_out_path_for(resource_or_scope)
+        # path to go to when you sign out
+        new_user_session_path
+    end
+
 end
